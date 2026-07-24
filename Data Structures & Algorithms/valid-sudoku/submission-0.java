71d9296bf050a72429bbb62d001f72a1dfdb8e26
Arpit-Git7 <arpit.singh2184@gmail.com>
2026-07-24 11:29:06 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row=new boolean[9][9];
+        boolean[][] col=new boolean[9][9];
+        boolean[][] box=new boolean[9][9];
+        for(int r=0;r<9;r++){
+            for(int c=0;c<9;c++){
+                if(board[r][c]=='.'){
+                    continue;
+                }
+                int val=board[r][c]-'1';
+                int boxval=(r/3)*3+(c/3);
+                if(row[r][val]||col[c][val]||box[boxval][val]){
+                    return false;
+                }
+                row[r][val]=true;
+                col[c][val]=true;
+                box[boxval][val]=true;
+            }
+        }
+        return true;
+    }
+}
