f10688ff60ac37ce824de0928e3c1ee6dc1a7c4d
Arpit-Git7 <arpit.singh2184@gmail.com>
2026-07-25 09:47:45 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] arr=s.toCharArray();
+        int n=arr.length;
+        int j=n-1;
+        for(int i=0;i<j;){
+            if(!Character.isLetterOrDigit(arr[i])){
+                i++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(arr[j])){
+                j--;
+                continue;
+            }
+            if(Character.toLowerCase(arr[i])!=Character.toLowerCase(arr[j])){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
